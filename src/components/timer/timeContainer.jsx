8c38b1fe3c6e8577824d567d2ee41957f35e04a0
Andres Fernandez <andres.fernandezcaballero@davinci.edu.ar>
2023-10-18 21:42:34 -0300

    ADD type error

--- src/components/timer/timeContainer.jsx
+++ src/components/timer/timeContainer.jsx
@@ -1,5 +1,11 @@
-const ItemTimeContainer = ({ itemTime }) => (
+const ItemTimeContainer = ({ itemTime }) => {
+
+  if(isNaN(itemTime)) 
+    throw new Error('itemTime must be a number');
+
+  return (
     <span>{itemTime < 10 ? `0${itemTime}` : itemTime}</span>
   );
+  }
 
   export default ItemTimeContainer;
